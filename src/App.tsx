e4d5b724f5f8279544663098fa807339cf80976a
JhoantanLop <jhooliveira.lopes@gmail.com>
2025-01-07 13:45:03 -0300

    implement caching for GitHub issues retrieval

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react';
+import getAllIssues from './services/Issues';
+import { Issue } from './interfaces/Issue';
 
 function App() {
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    async function fetchIssues() {
+      const issues = await getAllIssues('git-project-status', 'JhonatanLop');
+      console.log('Issues:', issues);
+      setIssues(issues);
+    }
+
+    fetchIssues();
+  }, []);
+
   return (
     <>
       <div>
         Iniciando projeto
       </div>
+      <div>
+        {issues.map(issue => (
+          <div key={issue.id}>{issue.title}</div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+
+export default App;
